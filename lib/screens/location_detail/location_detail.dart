715edab4046077985d5b1fe6540913d27f2ba766
Mijeong Ban <mban1@stevens.edu>
2020-10-15 14:04:28 -0400

    Working with models

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
+import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
     return Scaffold(
         appBar: AppBar(
-          title: Text('Hello'),
+          title: Text(location.name),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ImageBanner("assets/images/fuji-mountain.jpg"),
-            TextSection("summary1", "something1"),
-            TextSection("summary2", "something2"),
-            TextSection("summary3", "something3")
-          ],
-        ));
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ImageBanner(location.imagePath),
+              // Cascades: to invoke addAll() method
+            ]..addAll(textSections(location))));
+  }
+
+  List<Widget> textSections(Location location) {
+    // Transforming a List<LocationFact> into List<Widget>
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList(); // TextSection takes title and text
   }
 }
